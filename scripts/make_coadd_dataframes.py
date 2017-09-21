d760d63f13cd7e08bf7d52b8e325f507a7b8f82f
Jim Chiang <jchiang@slac.stanford.edu>
2017-09-21 15:40:19 -0700

    add calibrated magnitudes to the dataframes

--- scripts/make_coadd_dataframes.py
+++ scripts/make_coadd_dataframes.py
@@ -4,8 +4,10 @@ Convert Level 2 coadd catalogs to dask dataframes and write out to hdf.
 import sys
 import re
 import logging
+import numpy as np
 import pandas as pd
 import lsst.daf.persistence as dp
+import lsst.pex.exceptions as pexExcept
 
 logging.basicConfig(format="%(message)s", stream=sys.stdout)
 logger = logging.getLogger()
@@ -13,13 +15,42 @@ logger.setLevel(logging.INFO)
 #logger.setLevel(logging.DEBUG)
 
 def make_dataframe(catalog):
+    """
+    Create a data frame from a coadd catalog using the coadd catalog
+    schema.
+    """
     df = pd.DataFrame()
     for col in catalog.getSchema():
         name = col.field.getName()
         df[name] = catalog.get(name)
     return df
 
+def add_calibrated_mags(df, catalog, coadd):
+    """
+    Add columns for calibrated magnitudes for psfFlux and cmodelFlux.
+    """
+    modelFlux = catalog.getModelFlux()
+    psfFlux = catalog.getPsfFlux()
+    coadd_calib = coadd.getCalib()
+    modelMags = []
+    psfMags = []
+    for mflux, pflux in zip(modelFlux, psfFlux):
+        try:
+            modelMags.append(coadd_calib.getMagnitude(mflux))
+        except pexExcept.DomainError:
+            modelMags.append(np.nan)
+        try:
+            psfMags.append(coadd_calib.getMagnitude(pflux))
+        except pexExcept.DomainError:
+            psfMags.append(np.nan)
+    df['cmodelMag'] = np.array(modelMags)
+    df['psfMag'] = np.array(psfMags)
+    return df
+
 def get_patches(butler):
+    """
+    Get all of the patches in a discrete skymap.
+    """
     skymap = butler.get('deepCoadd_skyMap')
     tract = [x for x in skymap][0]
     patches = dict()
@@ -45,9 +76,11 @@ band = 'r'
 for patch in patches[tract]:
     dataId = dict(patch=patch, tract=tract, filter=band)
     try:
+        coadd = butler.get('deepCoadd', dataId=dataId)
         catalog = butler.get('deepCoadd_meas', dataId=dataId)
         logging.info("processing dataId: %s", dataId)
         df = make_dataframe(catalog)
+        df = add_calibrated_mags(df, catalog, coadd)
         df.to_hdf(hdf_file, key_prefix(patch), format='table')
     except RuntimeError as eobj:
         logger.debug("RuntimeError for patch %s:", patch)
